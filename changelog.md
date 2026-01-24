01ca6a393db4614c3e11e89cd10b031a25043858
ShuninYu <92670725+ShuninYu@users.noreply.github.com>
2026-01-25 05:46:26 +0800

    dev-push

---
+++ changelog.md
@@ -0,0 +1,29 @@
+# AnimeCursor
+[[简体中文]](#zh-cn)
+## Current Version
+- ### `0.1.3`
+    - Linked the npm package to the GitHub repository.
+
+## History Version
+- ### `0.1.2`
+    - `frames` is no longer a required parameter. Cursors without a `frames` setting will now default to not generating CSS animations.
+    - Added comments to the usage example in the README.
+- ### `0.1.1`
+    - Modified the image links in the README.
+- ### `0.1.0`
+    - Initial release.
+---
+<h1 id="zh-cn">AnimeCursor</h1>
+
+## 当前版本
+- ### `0.1.3`
+    - 为npm连接github仓库
+
+## 历史版本
+- ### `0.1.2`
+    - `frames`不再是必填项，现在没有`frames`的光标默认不生成CSS动画
+    - 为README中的使用示例添加了注释
+- ### `0.1.1`
+    - 修改README图片链接
+- ### `0.1.0`
+    - 首个版本
--- script.js
+++ script.js
@@ -1,33 +1,4 @@
 const fontLink = document.createElement('link');
 fontLink.rel = 'stylesheet';
-fontLink.href = '/fonts/chinese/result.css';
+fontLink.href = 'fonts/chinese/result.css';
 document.head.appendChild(fontLink);
-
-document.addEventListener('DOMContentLoaded', function() {
-    new AnimeCursor({
-        cursors: {
-            default: {
-                tags: ['body'],
-                size: [32,32],
-                image: 'i/cursor/cursor_default.gif',
-                pixel: true
-            },
-            pointer: {
-                tags: ['a', 'button'],
-                size: [32,36],
-                image: 'i/cursor/cursor_pointer.png',
-                frames: 3,
-                duration: 0.3,
-                pingpong: true,
-                offset: [10, 4],
-                pixel: true
-            },
-            text: {
-                tags: ['p', 'h1', 'h2', 'span'],
-                size: [32, 32],
-                image: 'i/cursor/cursor_text.png',
-                pixel: true
-            }
-        }
-    });
-})
